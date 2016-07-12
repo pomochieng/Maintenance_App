4b7fa6934c6ea6cabf6c6de8d1c4b929d7e51461
pomochieng <pomochieng@gmail.com>
2016-07-12 19:43:26 +0300

    filling in Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Returns the full title on a per-page basic.
+  def full_title(page_title = '')
+    base_title = "Maintenance App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
