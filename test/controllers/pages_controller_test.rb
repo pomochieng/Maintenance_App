c80b89aa128fff132b9867c6f39743f55614c8e3
pomochieng <pomochieng@gmail.com>
2016-07-12 18:21:33 +0300

    Fixing errors

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -7,31 +7,31 @@ class PagesControllerTest < ActionController::TestCase
   end
 
   test "should get dashboard" do
-    get pages_dashboard_url
+    get :dashboard
     assert_response :success
     assert_select "title", "Dashboard | #{@base_title}"
   end
 
   test "should get tickets" do
-    get pages_tickets_url
+    get :tickets
     assert_response :success
     assert_select "title", "Tickets | #{@base_title}"
   end
 
   test "should get knowledge_platform" do
-    get pages_knowledge_platform_url
+    get :knowledge_platform
     assert_response :success
     assert_select "title", "Knowledge_Platform | #{@base_title}"
   end
 
   test "should get reports" do
-    get pages_reports_url
+    get :reports
     assert_response :success
     assert_select "title", "Reports | #{@base_title}"
   end
 
   test "should get contact_us" do
-    get pages_contact_us_url
+    get :contact_us
     assert_response :success
     assert_select  "title", "Contact_Us | #{@base_title}"
   end
