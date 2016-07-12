a227fbb5d31a2f1111fb74ddbac6b866f34e6404
pomochieng <pomochieng@gmail.com>
2016-07-12 16:16:01 +0300

    Debugging Errors in Static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,29 +1,39 @@
 require 'test_helper'
 
-class PagesControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Maintenance App"
+  end
+
   test "should get dashboard" do
     get pages_dashboard_url
     assert_response :success
+    assert_select "title", "Dashboard | #{@base_title}"
   end
 
   test "should get tickets" do
     get pages_tickets_url
     assert_response :success
+    assert_select "title", "Tickets | #{@base_title}"
   end
 
   test "should get knowledge_platform" do
     get pages_knowledge_platform_url
     assert_response :success
+    assert_select "title", "Knowledge_Platform | #{@base_title}"
   end
 
   test "should get reports" do
     get pages_reports_url
     assert_response :success
+    assert_select "title", "Reports | #{@base_title}"
   end
 
   test "should get contact_us" do
     get pages_contact_us_url
     assert_response :success
+    assert_select  "title", "Contact_Us | #{@base_title}"
   end
 
 end
