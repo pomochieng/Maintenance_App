8bb19a2d25c5a26cfe20081942577f0b38833c88
pomochieng <pomochieng@gmail.com>
2016-07-16 15:06:49 +0300

    Signup Erroe Messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
     debbugger
   end
-  
   
   def new
     @user = User.new
@@ -18,12 +16,12 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
   private
 
   def user_params
-    params.require(:user0.permit(:name, :email, :password,
-                               :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
   end
-
 end
+
